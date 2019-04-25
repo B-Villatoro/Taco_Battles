9b458070a5614e4a295ff876ba73cd15602ee85f
motkilo <code22@motkilo.com>
2019-04-25 11:38:38 -0400

    added initial schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,35 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS taco_battle_db;
+CREATE DATABASE taco_battle_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+DROP DATABASE IF EXISTS taco_battle_db;
+CREATE DATABASE taco_battle_db;
+
+USE taco_battle_db;
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  name_user VARCHAR(100) NOT NULL,
+  taco_count INTEGER(10),
+  join_date DATETIME,
+  rating INTEGER(10),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE tacos (
+  id INT NOT NULL AUTO_INCREMENT,
+  taco_name VARCHAR(100),
+  ingredients VARCHAR(100),
+  attack INTEGER(10),
+  health INTEGER(10),
+  wins INTEGER(10),
+  losses INTEGER(10),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE ingredients (
+  id INT NOT NULL AUTO_INCREMENT,
+  ingred_name VARCHAR(50),
+  attack INTEGER(10),
+  health INTEGER(10),
+  PRIMARY KEY(id)
+);

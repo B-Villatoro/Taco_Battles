9c21d55f605ce18c207ab96e27172991d7aed38c
Mannan Qazi <mannanqazi2@gmail.com>
2019-04-27 21:20:12 -0400

    added logic to add a new taco to db

--- public/js/addTaco.js
+++ public/js/addTaco.js
@@ -1,26 +1,58 @@
 $(document).ready(function() {
+    // var option
     function getIngredients() {
         $.get('/api/ingredients', renderIngredientList);
     }
 
     function renderIngredientList(data) {
         console.log(data);
-        console.log(data[0]);
 
-        var i = 0;
-        createRow(data);
+        for (let i = 1; i < 4; i++) {
+            for (let j = 0; j < data.length; j++) {
+                let listOption = $('<option>');
+                listOption.attr('value', data[j].ingred_name);
+                listOption.attr(`data-attack`, data[j].attack);
+                listOption.attr(`data-health`, data[j].health);
+                listOption.html(data[j].ingred_name);
+                $(`#ing-${i}`).append(listOption);
+                $(`#ing-${i}`).prop('selectedIndex', -1);
+            }
+        }
     }
 
-    function createRow(ingredients) {
-        var i = 0;
-        do {
-            var listOption = $('<option>');
-            listOption.attr('value', ingredient[i].ingred_name);
-            $(`#ing-${i}`).append(listOption);
-            i++;
-        } while (i < ingredients.length);
-    }
     getIngredients();
 
-    // Add the taco to the database:
+    // post/add taco
+
+    $('.create-form').on('submit', postData);
+
+    function postData(event) {
+        event.preventDefault();
+        let totalAttack = 0;
+        let totalHealth = 0;
+        let selectorsArray = Array.from($('.form-control'));
+
+        let i = 1;
+        do {
+            totalAttack += $(`select#ing-${i}`)
+                .find('option:selected')
+                .data('attack');
+            totalHealth += $(`select#ing-${i}`)
+                .find('option:selected')
+                .data('health');
+            i++;
+        } while (i <= selectorsArray.length);
+
+        var tacoData = {
+            taco_name: $('#name').val(),
+            attack: totalAttack,
+            health: totalHealth,
+            userId: 1
+        };
+        console.log(tacoData);
+
+        $.post('/api/tacos', tacoData, function() {
+            window.location.href = '/';
+        });
+    }
 });

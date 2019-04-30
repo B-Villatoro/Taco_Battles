c516c0d8a4eff2bd2056e0adfb3a0e0e3dcadec3
Mannan Qazi <mannanqazi2@gmail.com>
2019-04-30 14:51:34 -0400

    updated battle logic

--- public/js/battlelogic.js
+++ public/js/battlelogic.js
@@ -1,74 +1,16 @@
 //Simple Version
 
-// function attack(UserTaco, EnemyTaco){
-//     if(UserTaco.attack > EnemyTaco.attack){
-//         UserTaco.wins++;
-//         EnemyTaco.losses++;
-//     }
-//     else{
-//         EnemyTaco.wins++;
-//         UserTaco.losses++;
-//     }
-// }
-
-// //example ingredient data
-// let ingredients = {
-//     Chicken: {
-//         attack: 20,
-//         health: 20,
-//     },
-//     Lettuce: {
-//         attack: 10,
-//         health: 40,
-//     },
-//     Tomatos: {
-//         attack: 10,
-//         health: 40,
-//     }
-// }
-// // example taco object
-// let TacoEx = {
-//     name: "Tacquito",
-//     ingredients: [ingredients.Chicken, ingredients.Lettuce, ingredients.Tomatos],
-//     attack: ingredients.Chicken.attack + ingredients.Lettuce.attack + ingredients.Tomatos.attack,
-//     health: ingredients.Chicken.health + ingredients.Lettuce.health + ingredients.Tomatos.health,
-//     wins: 0,
-//     losses: 0,
-//     UserId: 0001,
-// }
-
-// //other taco
-// let TacoEn = {
-
-//     name: "Tacoooooo",
-//     attack: ingredients.Chicken.attack + ingredients.Chicken.attack + ingredients.Tomatos.attack,
-//     health: ingredients.Chicken.health + ingredients.Chicken.health + ingredients.Tomatos.health,
-//     wins: 0,
-//     losses: 0,
-//     UserId: 0001,
-// }
-
 //attack function
 function attack(taco1, taco2) {
     taco1.health -= taco2.attack;
     taco2.health -= taco1.attack;
 
-    // console.log(userTaco);
-    // console.log(enemyTaco);
+    console.log(taco1);
+    console.log(taco2);
 
-    // while (enemyTaco.health > 0 && userTaco.health > 0) {
-    //     attack(userTaco, enemyTaco);
-    // }
-    // if (enemyTaco.health <= 0) {
-    //     // userTaco.wins++;
-    //     // enemyTaco.losses++;
-    //     console.log('user taco wins');
-    //     $('#versus').text(`${userTaco.name} wins`);
-    // } else if (userTaco.health <= 0) {
-    //     // userTaco.losses++;
-    //     // enemyTaco.wins++;
-    //     $('#versus').text(`${enemyTaco.name} wins`);
-    // }
+    while (taco1.health > 0 && taco2.health > 0) {
+        attack(taco1, taco2);
+    }
 }
 $(document).ready(function() {
     $(document).on('click', '.attackTaco', function() {
@@ -100,20 +42,7 @@ $(document).ready(function() {
 
         console.log(enemyTaco);
 
-        if (userTaco.health > 0) {
-            attack(userTaco, enemyTaco);
-        }
-
         attack(userTaco, enemyTaco);
-
-        // if (userTaco.health > 0) {
-        //     attack(userTaco, enemyTaco);
-        //     // $(`.${enemyTaco.name}-health`).text(`${enemyTaco.health}`);
-        // }
-        // if (enemyTaco.health > 0) {
-        //     attack(enemyTaco, userTaco);
-        //     // $(`.${userTaco.name}-health`).text(`${userTaco.health}`);
-        // }
     });
 });
 

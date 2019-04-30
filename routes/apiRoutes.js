1c56d7c41792a738f0dffa09c980663617018535
Mannan Qazi <mannanqazi2@gmail.com>
2019-04-30 11:47:46 -0400

    In battleground, user list is displayed and the tacos associated with the user

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,43 +1,66 @@
-var db = require("../models");
+var db = require('../models');
 
 module.exports = function(app) {
-  // Get all Tacos
-  app.get("/api/tacos", function(req, res) {
-    db.taco.findAll({}).then(function(dbTacos) {
-      res.json(dbTacos);
+    // Get all Tacos
+    app.get('/api/tacos', function(req, res) {
+        db.taco.findAll({}).then(function(dbTacos) {
+            res.json(dbTacos);
+        });
     });
-  });
-  //Get taco by id
-  app.get("/api/tacos/:id", function(req, res) {
-    db.taco.findOne({ where: { id: req.params.id } }).then(function(dbTaco) {
-      res.json(dbTaco);
+    //Get taco by id
+    app.get('/api/tacos/:id', function(req, res) {
+        db.taco
+            .findOne({ where: { id: req.params.id } })
+            .then(function(dbTaco) {
+                res.json(dbTaco);
+            });
     });
-  });
 
-  // Create a new Taco
-  app.post("/api/tacos", function(req, res) {
-    db.taco.create(req.body).then(function(dbTaco) {
-      res.json(dbTaco);
+    // Create a new Taco
+    app.post('/api/tacos', function(req, res) {
+        db.taco.create(req.body).then(function(dbTaco) {
+            res.json(dbTaco);
+        });
     });
-  });
 
-  // Delete an Taco by id
-  app.delete("/api/tacos/:id", function(req, res) {
-    db.taco.destroy({ where: { id: req.params.id } }).then(function(dbTaco) {
-      res.json(dbTaco);
+    // Delete an Taco by id
+    app.delete('/api/tacos/:id', function(req, res) {
+        db.taco
+            .destroy({ where: { id: req.params.id } })
+            .then(function(dbTaco) {
+                res.json(dbTaco);
+            });
     });
-  });
-  
-  //get all ingredients
-  app.get("/api/ingredients", function(req, res) {
-    db.ingredient.findAll({}).then(function(dbIngredients) {
-      res.json(dbIngredients);
-    });
-  });
 
-  app.get("/api/users", function(req, res) {
-    db.user.findAll({}).then(function(dbUser) {
-      res.json(dbUser);
+    //get all ingredients
+    app.get('/api/ingredients', function(req, res) {
+        db.ingredient.findAll({}).then(function(dbIngredients) {
+            res.json(dbIngredients);
+        });
     });
-  });
+
+    app.get('/api/users', function(req, res) {
+        db.user
+            .findAll({
+                include: [db.taco]
+            })
+            .then(function(dbUser) {
+                res.json(dbUser);
+            });
+    });
+
+    app.get('/api/users/:id', function(req, res) {
+        db.user
+            .findAll({
+                where: {
+                    id: req.params.id
+                },
+                include: [db.taco]
+            })
+            .then(function(dbUser) {
+                res.json(dbUser);
+            });
+    });
+
+    //   route for getting tacos associated with a user
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,65 +1,60 @@
-let db = require("../models");
+let db = require('../models');
 
-module.exports = function (app) {
-  // Load index page
-  app.get("/", function (req, res) {
-    // db.taco.findAll({}).then(function(dbTacos) {
-    res.render("index", {
-      // msg: "Hello",
-      // Tacos: dbTacos
+module.exports = function(app) {
+    // Load index page
+    app.get('/', function(req, res) {
+        // db.taco.findAll({}).then(function(dbTacos) {
+        res.render('index', {
+            // msg: "Hello",
+            // Tacos: dbTacos
+        });
+        // });
     });
-    // });
-  });
 
-  app.get("/profile", function (req, res) {
-    // db.taco.findAll({}).then(function(dbTacos) {
-    res.render("profile", {
-      // msg: "Hello",
-      // Tacos: dbTacos
+    app.get('/profile', function(req, res) {
+        // db.taco.findAll({}).then(function(dbTacos) {
+        res.render('profile', {
+            // msg: "Hello",
+            // Tacos: dbTacos
+        });
+        // });
     });
-    // });
-  });
 
-  app.get("/battle", function (req, res) {
-    db.taco.findAll({}).then(function (dbTacos) {
-      db.user.findAll({}).then(function (dbUsers) {
-        console.log(dbUsers);
-        let obj = {
-          users: dbUsers,
-          allTaco: dbTacos,
-          myTaco: dbTacos[0],
-          enemy: dbTacos[1]
-        }
-        res.render("battleground", obj);
-      });//end db user
+    app.get('/battle', function(req, res) {
+        db.taco.findAll({}).then(function(dbTacos) {
+            db.user.findAll({}).then(function(dbUsers) {
+                console.log(dbUsers);
+                let obj = {
+                    users: dbUsers,
+                    allTaco: dbTacos
+                };
+                res.render('battleground', obj);
+            }); //end db user
+        }); //end db taco
+    }); //end app.get
 
-    });//end db taco
-
-
-
-
-  });//end app.get
-
-  app.get("/taco", function (req, res) {
-    // db.taco.findAll({}).then(function(dbTacos) {
-    res.render("createtaco", {
-      // msg: "Hello",
-      // Tacos: dbTacos
+    app.get('/taco', function(req, res) {
+        // db.taco.findAll({}).then(function(dbTacos) {
+        res.render('createtaco', {
+            // msg: "Hello",
+            // Tacos: dbTacos
+        });
+        // });
     });
-    // });
-  });
 
-  // Load Taco page and pass in an Taco by id
-  app.get("/taco/:id", function (req, res) {
-    db.taco.findOne({ where: { id: req.params.id } }).then(function (dbTaco) {
-      res.render("Taco", {
-        Taco: dbTaco
-      });
+    // Load Taco page and pass in an Taco by id
+    app.get('/taco/:id', function(req, res) {
+        db.taco
+            .findOne({ where: { id: req.params.id } })
+            .then(function(dbTaco) {
+                res.render('Taco', {
+                    Taco: dbTaco
+                });
+            });
     });
-  });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
-    res.render("404");
-  });
+    // Render 404 page for any unmatched routes
+    app.get('*', function(req, res) {
+        res.render('404');
+    });
 };

1a126cbb689a92ddc08a778da70bacb6d97ee300
motkilo <code22@motkilo.com>
2019-04-30 11:15:32 -0400

    added ingredients list and attack values

--- database/seeds.sql
+++ database/seeds.sql
@@ -1,8 +1,29 @@
 USE taco_battle_db;
 
-INSERT INTO users (name_user)
+INSERT INTO ingredients (ingred_name, attack)
 VALUES 
-  ("Tim"),
-  ("Mannan"),
-  ("Bryan"),
-  ("Keannu");
+  ("hard shell", 20),
+  ("soft shell", 20),
+  ("rice", 10),
+  ("lettuce", 10),
+  ("limes", 10),
+  ("shredded cheese", 15),
+  ("pico de gallo", 25),
+  ("salsa verde", 30),
+  ("sour cream", 25),
+  ("refried beans", 25),
+  ("black beans", 25),
+  ("guacamole", 75),
+  ("jalapeno peppers", 100),
+  ("habanero peppers", 250),
+  ("steak", 500),
+  ("ground beef", 400),
+  ("chicken", 400),
+  ("pork", 400),
+  ("chorizo", 425),
+  ("duck", 375),
+  ("fish", 350),
+  ("shrimp", 325),
+  ("bacon", 200),
+  ("Guatemalan Insanity Pepper", 1000),
+  ("Ghost Pepper", 750),

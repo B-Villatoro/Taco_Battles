277ea0a0e6205732162e8c8a823a5808c4bf27da
Bryan Villatoro <bvillatoro@ymail.com>
2019-05-01 10:15:48 -0400

    fix modal

--- public/js/battlelogic.js
+++ public/js/battlelogic.js
@@ -74,7 +74,7 @@ $(document).ready(function () {
             let iframe = $(
                 '<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/l2JIl2RIdkUg4skCs" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/tacobell-taco-tacos-bell-l2JIl2RIdkUg4skCs">via GIPHY</a></p>'
             );
-
+            modalBody.empty();
             modalBody.append(iframe);
             $('#myModal').modal('show');
             
